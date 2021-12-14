657e0c5a6d1d07224ca3e3a82deb9fd16a418688
mayusha <mayushachowdary95@gmail.com>
2021-12-14 21:47:34 +0530

    hi

---
+++ assign.js
@@ -0,0 +1,44 @@
+
+var num1=30;
+var num2=40;
+var num3=50;
+
+
+num1 += 10;
+console.log(num1);
+
+
+num3 -=10 ;
+console.log(num3);
+
+num2 *= 5;
+console.log(num2);
+
+
+ 
+num1 /= 5;
+console.log(num1);
+
+
+
+num2 %= 10;
+console.log(num2);
+
+
+var num=10;
+num <<=10;
+console.log(num);
+
+
+var num4=6;
+num4 >>=6;
+console.log(num4);
+
+var num5=10;
+num5 &=5;
+console.log(num5);
+
+
+var num6=15;
+num6 ^=10;
+console.log(num6);
